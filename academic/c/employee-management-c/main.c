f5778d40620ebea7b9e4ce6fd2b5844b9e7f3676
LouasDev <louasdev@gmail.com>
2026-07-04 15:06:33 -0300

    Adiciona comentarios explicativos e ajustes finais

--- academic/c/employee-management-c/main.c
+++ academic/c/employee-management-c/main.c
@@ -22,11 +22,11 @@ void cadastrar(char nomes[][50], char emails[][30], float salarios[], int *qtd) 
         (*qtd)++;
         // aki foi utilizado (*qtd)++ e não qtd++ porque precisamos aumentar o valor guardado no endereço,
         // não fazer o ponteiro apontar para a próxima posição de memória.
-
-        // detalhe: usei scanf por escultar que nao era pra usar fgets( escutei laercio conversando com alguem sobre
-        // usando o filtro "%49[^\n]" funciona perfeitamente sem o colateral do fgets de de guardar o caractere de
+        //
+        // detalhe: usei scanf por escutar que nao era pra usar fgets( escutei laercio conversando com alguem sobre )
+        // usando o filtro "%49[^\n]" funciona perfeitamente sem o colateral do fgets de guardar o caractere de
         // quebra de linha (\n) junto da string.
-
+        //
         //aki teria muitas formas de "pegar" esse valor fgets, gets, scanset
     } else {
         printf("Numero Maximo de cadastros atingidos");
@@ -36,6 +36,7 @@ void cadastrar(char nomes[][50], char emails[][30], float salarios[], int *qtd) 
 // Função para mostrar todos os cadastros
 void mostrarTodos(char nomes[][50], char emails[][30], float salarios[], int qtd) {
     // filtro para ver se tem algum usuario cadastrado, se nao tiver ele nao executa a busca
+    // se nao tiver pessoas cadastrada ele ja finaliza pra economizar memoria/tempo
     if (qtd == 0) {
         printf("Nenhum funcionário cadastrado ainda.\n");
     } else {
@@ -55,14 +56,12 @@ void mostrarTodos(char nomes[][50], char emails[][30], float salarios[], int qtd
 
 //Funcao responsavel por buscar e mostrar os dados
 void buscar(char nomes[][50], char emails[][30], float salarios[], int qtd) {
-    // aki tem alguns detalhes
-    // 1. a variavel encontrado, funciona como um log, um retorno, basicamente ela vai ser responsavel por dizer
-    // se o nome/cadastro nao foi encontrado
-    // ela começa com 0 que significa  nao encontrado e so muda apos encontrar o cadastro
+    // Detalhe:  a variavel encontrado, funciona como um log, um retorno, basicamente ela vai ser responsavel por dizer
+    // se o nome/cadastro NAO foi encontrado
+    // ela começa com 0, que significa nao encontrado e so muda (para "1") apos encontrar o cadastro ser encontrado
     int encontrado = 0;
 
     if (qtd == 0) {
-        // se nao tiver pessoas cadastrada ele ja finaliza pra economizar memoria/tempo
         printf("Nenhum funcionário cadastrado ainda.\n");
     } else {
         char nomeBusca[50];
@@ -90,6 +89,7 @@ void buscar(char nomes[][50], char emails[][30], float salarios[], int qtd) {
 // Detalhe: ate que se prove o contrario o primeiro cadastro (indice = 0) começa sendo o maior salario
 // isso economiza 1 comparação, fica um pouco mais rapido, alem de evitar redundancia, pq evita comparar ele com ele mesmo
 void maior(char nomes[][50], char emails[][30], float salarios[], int qtd) {
+
     if (qtd == 0) {
         printf("Nenhum funcionário cadastrado ainda.\n");
     } else {
@@ -98,7 +98,7 @@ void maior(char nomes[][50], char emails[][30], float salarios[], int qtd) {
             if (salarios[i] > salarios[indiceMaior]) {
                 indiceMaior = i;
                 // Outro detalhe, aki e guardado o indice(nao o valor do indice), porque assim no final
-                // conseguimos  acessar nome, email e salário da mesma pessoa usando esse único índice.
+                // conseguimos acessar nome, email e salário da mesma pessoa usando esse único índice.
             }
         }
         // aki:
@@ -110,8 +110,12 @@ void maior(char nomes[][50], char emails[][30], float salarios[], int qtd) {
     }
 }
 
-//função responsavel por mostrar a media de salarios
+// função responsavel por mostrar a media de salarios
+// aki nao e necessario passar como parametro nomes e emails, deixei
+// porque o enunciado pede (Cada opção deve,obrigatoriamente, chamar uma função
+// específica passando as matrizes e vetores como parâmetros.)
 void media(char nomes[][50], char emails[][30], float salarios[], int qtd) {
+
     if (qtd == 0) {
         printf("Nenhum funcionário cadastrado ainda.\n");
     } else {
@@ -120,32 +124,33 @@ void media(char nomes[][50], char emails[][30], float salarios[], int qtd) {
             valores += salarios[i];
         }
         mediaFinal = valores / qtd;
-        // como temos essa varial qtd, que so e mudada quando e cadastrado 1 pessoa
+        // como temos essa variavel qtd, que so e mudada quando e cadastrado 1 pessoa
         // fica seguro usar ela pra tirar a media.
         printf("A media e: %.2f", mediaFinal);
     }
 }
 
 int main(void) {
-    //variaveis
+    //variaveis Utilzadas
     char nomes[10][50];
     char email[10][30];
     float salario[10];
     int op, qtd = 0;
-
-    //menu aesthetic
-
+    // op nao esta zerada por conta do do-while(que verifica depois)
     do {
         // Para melhor organização do codigo(na hora de iniciar eu utilizei esse simples sisteminha de limpeza
         // basicamente ele vai decidir atravez do if se voce esta no windows ou linux, e vai limpar o console
-        // DETALHE: Dependendo do terminal que voce usar isso nao funciona, exemplo: se voce usar o terminal
+        //
+        // DETALHE: Dependendo do terminal que voce usar isso nao funciona, se voce usar o terminal
         // proprio do linux ou windows, funciona, entretanto dependendo da ide nao vai funcionar( exemplo no clion)
+        // nao funciona por conta do terminal da ide ( meio que essas ide simula um terminal)
 #ifdef _WIN32
         system("cls");
 #else
         system("clear");
 #endif
 
+        //menu aesthetic
         printf("-- Sistema de Cadastro é Agenda -- \n");
         printf("* 1 Para Cadastrar Novo Funcionario \n");
         printf("* 2 para Mostrar Todos os Dados \n");
@@ -166,7 +171,8 @@ int main(void) {
                 break;
             case 3:
                 buscar(nomes, email, salario, qtd);
-
+                // essa linha so entrega os dados e chama a função, os printf que aparecem
+                // na tela estao dentro do corpo da função buscar(), la em cima no arquivo
                 break;
             case 4:
                 maior(nomes, email, salario, qtd);
@@ -181,23 +187,30 @@ int main(void) {
                     printf("Opçao Invalida \n");
                 }
                 // responsavel por retornar log, caso a pessoa digite qualquer valor que nao seja entre 0-6
+                //
                 // Detalhe: o if tem função de melhor visualizaçã do que esta acontecendo
                 // se nao tivesse, so tivesse "printf("Opçao Invalida \n");" e a pessoa apertaçe 6 para sair
                 // ele ia falar opção invalida e finalizar, com esse if caso a pessoa aperta 6, ele printa Saindo
-                // basicamente o if, so esta deixando mais visivel oq esta acontecendo
+                // basicamente o if, so esta deixando mais visivel oque esta acontecendo
                 break;
         }
         printf("\nPressione Enter para continuar...");
         getchar();
         getchar();
-        // explicação: como o loop so termina apos a pessoa digitar 6, ele meio que fica spamando, ele ja pula
-        // para o menu, entao utilizei o getchar(afim da pessoa apertar enter), para que ele nao pule imediatamente
-        // para o menu, pensei em usar sleep ( que so nao se aplica, pq eu estou usando linux pra programar, e provavelmente
-        // laercio usa windows(resumindo nao sei onde vai ser testado), eu decidir usar o getchar ( que vai pegar 1 caracter)
-        // basicamente um botao de continuar
+        // Explicação: Sem essa pausa, o loop já reimprimiria o menu na sequência, sem dar tempo
+        // de ler o resultado da opção escolhida.
+        //
+        // Utilizei getchar() (funciona em qualquer sistema, ide, não depende do SO) em vez de sleep() ou
+        // system("pause"), que necessitaria saber antecipadamente se o programa
+        // vai rodar em Windows ou Linux ( pode ser que ocorra algum erro desnecessario )
+        //
+        // São necessários DOIS getchar() o primeiro consome o "\n" deixado no
+        // buffer pelo scanf anterior e o segundo é quem realmente espera o
+        // usuário apertar Enter para continuar.
     } while (op != 6);
 
-    // decidir utilizar o do while por ele consultar no final,
+    // decidir utilizar o do while por ele consultar no final
+    //
     // alem de ser mais estetico(nao sei por que eu gosto muito do do-while)
     return 0;
 }
